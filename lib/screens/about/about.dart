2f5b851c9b7b298b8d5c473ef84e8c4a1473ff40
Sreeram3927 <sreeram292004@gmail.com>
2022-12-18 16:46:09 +0530

    about page starting

---
+++ lib/screens/about/about.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class About extends StatelessWidget {
+  const About({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 7.0),
+      child: Container(
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            border: Border.all(width: 3.5, color: Colors.black54),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column()),
+    );
+  }
+}
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:infinity_project/screens/about/about.dart';
 import 'timetable/timetable.dart';
 
-
 class MainScreen extends StatelessWidget {
-
   static ValueNotifier<int> idx = ValueNotifier(0);
 
   final screens = const [
     TimeTable(),
-    Center(child: Text('Reminders', style: TextStyle(fontSize: 80),),),
-    Center(child: Text('Settings', style: TextStyle(fontSize: 80),),),
-    Center(child: Text('About', style: TextStyle(fontSize: 80),),),
+    Center(
+      child: Text(
+        'Reminders',
+        style: TextStyle(fontSize: 80),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Settings',
+        style: TextStyle(fontSize: 80),
+      ),
+    ),
+    About(),
   ];
 
   const MainScreen({super.key});
@@ -20,15 +29,10 @@ class MainScreen extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 3.0, horizontal: 15.0),
       child: ValueListenableBuilder(
-
         valueListenable: idx,
-
         builder: (context, value, child) {
-
           return screens[value];
-          
         },
-
       ),
     );
   }
