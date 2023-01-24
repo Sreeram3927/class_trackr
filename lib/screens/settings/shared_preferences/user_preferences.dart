9302793e74a365b67f9057de598ac2f984308b8a
Sreeram3927 <sreeram292004@gmail.com>
2023-01-24 20:27:09 +0530

    initializing shared_preferences

--- lib/screens/settings/shared_preferences/user_preferences.dart
+++ lib/screens/settings/shared_preferences/user_preferences.dart
@@ -1,3 +1,10 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class UserPreferences {
-  
+
+  static SharedPreferences _preferences;
+
+  static Future init() async => _preferences = await SharedPreferences.getInstance();
+
+
 }
