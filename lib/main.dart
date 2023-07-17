868bcb51c22fb589f94d9484db17b35ec10a66be
Sreeram3927 <sreeram292004@gmail.com>
2023-07-17 23:50:36 +0530

    add analytics andd change minSdkVersion

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'firebase/firebase_options.dart';
 import 'package:infinity_project/data/user_preferences.dart';
 import 'home/home.dart';
 
