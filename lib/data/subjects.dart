11c1ee2844666d9ac484b7ace249c271ba7edc99
Sreeram3927 <sreeram292004@gmail.com>
2023-07-21 09:09:23 +0530

    add the slots for subjects

--- lib/data/subjects.dart
+++ lib/data/subjects.dart
@@ -3,54 +3,81 @@ class Subject {
 
   static Map subData = {
 
-    '21MAB201T' : ['Transforms and Boundary Value Problems', ],
+    '21MAB201T' : {
+      'name' : 'Transforms and Boundary Value Problems',
+      'slots' : ['A',], 
+    },
 
-    '21MHC203J' : ['Fluid power system and Automation', ],
+    '21MHC203J' : {
+      'name' : 'Fluid power system and Automation',
+      'slots' : ['B',], 
+    },
 
-    '21MHC202J' : ['Analog and Digital Electronics', ],
+    '21MHC202J' : {
+      'name' : 'Analog and Digital Electronics',
+      'slots' : ['C',], 
+    },
 
-    '21MHS201T' : ['Thermodynamics and Heat Transfer', ],
+    '21MHS201T' : {
+      'name' : 'Thermodynamics and Heat Transfer',
+      'slots' : ['D',], 
+    },
 
-    '21PDH201T' : ['Social Engineering', ],
+    '21PDH201T' : {
+      'name' : 'Social Engineering',
+      'slots' : ['E',], 
+    },
 
-    '21MHC201T' : ['Electrical Actuators and Drives', ],
+    '21MHC201T' : {
+      'name' : 'Electrical Actuators and Drives',
+      'slots' : ['F',], 
+    },
 
-    '21LEM202T' : ['UHV-II: Universal Human Values – Understanding Harmony and Ethical Human Conduct', ],
+    '21LEM202T' : {
+      'name' : 'UHV-II: Universal Human Values – Understanding Harmony and Ethical Human Conduct',
+      'slots' : ['G',], 
+    },
 
-    '21LEM201T' : ['Professional Ethics', ],
+    '21LEM201T' : {
+      'name' : 'Professional Ethics',
+      'slots' : ['P38'], 
+    },
 
-    '21PDM201L' : ['Verbal Reasoning', ],
+    '21PDM201L' : {
+      'name' : 'Verbal Reasoning',
+      'slots' : ['P7', 'P8'], 
+    },
 
 
     //First Year Courses
 
-    '21MHC101P' : ['Elements of Mechatronics Systems', ],
+    // '21MHC101P' : ['Elements of Mechatronics Systems', ],
 
-    '21CSS101J' : ['Programming for Problem Solving', ],
+    // '21CSS101J' : ['Programming for Problem Solving', ],
 
-    '21GNH101J' : ['Philosophy of Engineering'],
+    // '21GNH101J' : ['Philosophy of Engineering'],
 
-    '21MAB102T' : ['Advanced Calculus and Complex Analysis'],
+    // '21MAB102T' : ['Advanced Calculus and Complex Analysis'],
 
-    '21BTB103T' : ['Biology'],
+    // '21BTB103T' : ['Biology'],
 
-    '21CYB101J' : ['Chemisty'],
+    // '21CYB101J' : ['Chemisty'],
 
-    '21MES102L' : ['Basic Civil and Mechanical Workshop'],
+    // '21MES102L' : ['Basic Civil and Mechanical Workshop'],
 
-    '21PDM102L' : ['General Aptitude'],
+    // '21PDM102L' : ['General Aptitude'],
 
-    'GEM' : ['German', '21xxx10xT',],
+    // 'GEM' : ['German', '21xxx10xT',],
 
-    'FNH' : ['French', '21xxx10xT',],
+    // 'FNH' : ['French', '21xxx10xT',],
 
-    'SPH' : ['Spanish', '21xxx10xT',],
+    // 'SPH' : ['Spanish', '21xxx10xT',],
 
-    'CNE' : ['Chinese', '21xxx10xT',],
+    // 'CNE' : ['Chinese', '21xxx10xT',],
 
-    'KRN' : ['Korean', '21xxx10xT',],
+    // 'KRN' : ['Korean', '21xxx10xT',],
 
-    'JPN' : ['Japanese', '21xxx10xT',],
+    // 'JPN' : ['Japanese', '21xxx10xT',],
 
     '420' : ['Subject Not Found!!', 'Subject Code Not Found',]
   };
