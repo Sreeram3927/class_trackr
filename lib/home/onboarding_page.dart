c6aaaa27c286b4b7c234ca649a28c561c1c0d64a
Sreeram3927 <sreeram292004@gmail.com>
2023-07-23 12:27:47 +0530

    create course selection screen

--- lib/home/onboarding_page.dart
+++ lib/home/onboarding_page.dart
@@ -1,17 +1,5 @@
 import 'package:flutter/material.dart';
-
-// class OnboardingPage extends StatelessWidget {
-//   const OnboardingPage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Scaffold(
-//       body: Center(
-//         child: Text('Onboarding Page'),
-//       ),
-//     );
-//   }
-// }
+import 'package:infinity_project/data/timetable_data.dart';
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
@@ -30,7 +18,7 @@ class _OnboardingPageState extends State<OnboardingPage> {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -44,13 +32,11 @@ class _OnboardingPageState extends State<OnboardingPage> {
                     });
                   },
                   children: [
-                    Center(
-                      child: Text('Page 1'),
-                    ),
-                    Center(
+                    CourseSelectionPage(),
+                    const Center(
                       child: Text('Page 2'),
                     ),
-                    Center(
+                    const Center(
                       child: Text('Page 3'),
                     ),
                   ],
@@ -84,6 +70,72 @@ class _OnboardingPageState extends State<OnboardingPage> {
             ],
           ),
         ),
+      )
+    );
+  }
+}
+
+
+class CourseSelectionPage extends StatefulWidget {
+  const CourseSelectionPage({super.key});
+
+  @override
+  State<CourseSelectionPage> createState() => _CourseSelectionPageState();
+}
+
+class _CourseSelectionPageState extends State<CourseSelectionPage> {
+  String? _selectedCourse;
+
+  List courses = TimeTableData.courses;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          DropdownButtonFormField<String>(
+            value: _selectedCourse,
+            onChanged: (newValue) {
+              setState(() {
+                _selectedCourse = newValue;
+              });
+            },
+            items: courses.map((course) {
+              return DropdownMenuItem<String>(
+                value: course,
+                child: Text(course),
+              );
+            }).toList(),
+            decoration: const InputDecoration(
+              labelText: 'Select Course',
+              border: OutlineInputBorder(),
+            ),
+          ),
+
+          const SizedBox(height: 16),
+
+          ElevatedButton(
+            onPressed: () {
+              // Handle the course selection and navigate to the next screen
+              if (_selectedCourse != null) {
+                // Do something with the selected course
+                print('Selected course: $_selectedCourse');
+                // Navigate to the next screen or perform any other action
+                // Example:
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => NextScreen()),
+                // );
+              } else {
+                // Show a message to ask the user to select a course
+                print('Please select a course before proceeding.');
+              }
+            },
+            child: const Text('Next'),
+          ),
+        ],
       )
     );
   }
