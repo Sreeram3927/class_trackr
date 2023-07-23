5491ba2820ed94c0e731347438d51b8fb7b08799
Sreeram3927 <sreeram292004@gmail.com>
2023-07-23 13:09:57 +0530

    store the selected data in UserPreferences

--- lib/home/onboarding_page.dart
+++ lib/home/onboarding_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:infinity_project/data/subjects.dart';
 import 'package:infinity_project/data/timetable_data.dart';
 
 class OnboardingPage extends StatefulWidget {
@@ -13,9 +14,26 @@ class _OnboardingPageState extends State<OnboardingPage> {
   final PageController _pageController = PageController(initialPage: 0);
   int _currentPage = 0;
 
+  void nextScreen() {
+    _pageController.animateToPage(
+      _currentPage - 1,
+      duration: const Duration(milliseconds: 400),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  void previousScreen() {
+    _pageController.animateToPage(
+      _currentPage + 1,
+      duration: const Duration(milliseconds: 400),
+      curve: Curves.easeInOut,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFCCDAD1),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
@@ -35,9 +53,12 @@ class _OnboardingPageState extends State<OnboardingPage> {
                     DataSelectionPage(
                       dataList: TimeTableData.courses,
                       dataKey: 'course',
+                      selectedData: TimeTableData.currentCourse,
                     ),
-                    const Center(
-                      child: Text('Page 2'),
+                    DataSelectionPage(
+                      dataList: Subject.batches,
+                      dataKey: 'batch',
+                      selectedData: Subject.currentBatch,
                     ),
                     const Center(
                       child: Text('Page 3'),
@@ -49,23 +70,11 @@ class _OnboardingPageState extends State<OnboardingPage> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   TextButton(
-                    onPressed: () {
-                      _pageController.animateToPage(
-                        _currentPage - 1,
-                        duration: const Duration(milliseconds: 400),
-                        curve: Curves.easeInOut,
-                      );
-                    },
+                    onPressed: nextScreen,
                     child: const Text('Previous'),
                   ),
                   TextButton(
-                    onPressed: () {
-                      _pageController.animateToPage(
-                        _currentPage + 1,
-                        duration: const Duration(milliseconds: 400),
-                        curve: Curves.easeInOut,
-                      );
-                    },
+                    onPressed: previousScreen,
                     child: const Text('Next'),
                   ),
                 ],
@@ -82,10 +91,13 @@ class _OnboardingPageState extends State<OnboardingPage> {
 class DataSelectionPage extends StatefulWidget {
   final List dataList;
   final String dataKey;
+  final String? selectedData;
+
   const DataSelectionPage({
     super.key,
     required this.dataList,
     required this.dataKey,
+    required this.selectedData,
   });
 
   @override
@@ -93,69 +105,73 @@ class DataSelectionPage extends StatefulWidget {
 }
 
 class _DataSelectionPageState extends State<DataSelectionPage> {
-  String? _selectedData;
+  String? _newData;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-
-          Text(
-            'Select ${widget.dataKey}',
-            style: const TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+    
+            Text(
+              'Select ${widget.dataKey}',
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color:  Color(0xFF38302E)
+              ),
+              textAlign: TextAlign.center,
             ),
-            textAlign: TextAlign.center,
-          ),
-
-          const SizedBox(height: 16),
-
-          DropdownButtonFormField<String>(
-            value: _selectedData,
-            onChanged: (newValue) {
-              setState(() {
-                _selectedData = newValue;
-              });
-            },
-            items: widget.dataList.map((course) {
-              return DropdownMenuItem<String>(
-                value: course,
-                child: Text(course),
-              );
-            }).toList(),
-            decoration: InputDecoration(
-              labelText: 'Select ${widget.dataKey}',
-              border: const OutlineInputBorder(),
+    
+            const SizedBox(height: 16),
+    
+            DropdownButtonFormField<String>(
+              value: _newData ?? widget.selectedData,
+              onChanged: (newValue) {
+                setState(() {
+                  _newData = newValue;
+                });
+              },
+              items: widget.dataList.map((course) {
+                return DropdownMenuItem<String>(
+                  value: course,
+                  child: Text(course),
+                );
+              }).toList(),
+              decoration: InputDecoration(
+                labelText: 'Select ${widget.dataKey}',
+                border: const OutlineInputBorder(),
+              ),
             ),
-          ),
-
-          const SizedBox(height: 16),
-
-          ElevatedButton(
-            onPressed: () {
-              // Handle the course selection and navigate to the next screen
-              if (_selectedData != null) {
-                // Do something with the selected course
-                print('Selected course: $_selectedData');
-                // Navigate to the next screen or perform any other action
-                // Example:
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(builder: (context) => NextScreen()),
-                // );
-              } else {
-                // Show a message to ask the user to select a course
-                print('Please select a ${widget.dataKey} before proceeding.');
-              }
-            },
-            child: const Text('Next'),
-          ),
-        ],
-      )
+    
+            const SizedBox(height: 16),
+    
+            ElevatedButton(
+              onPressed: () {
+                // Handle the course selection and navigate to the next screen
+                if (_newData != null) {
+                  // Do something with the selected course
+                  print('Selected course: $_newData');
+                  // Navigate to the next screen or perform any other action
+                  // Example:
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => NextScreen()),
+                  // );
+                } else {
+                  // Show a message to ask the user to select a course
+                  print('Please select a ${widget.dataKey} before proceeding.');
+                }
+              },
+              child: const Text('Next'),
+            ),
+          ],
+        )
+      ),
     );
   }
 }
