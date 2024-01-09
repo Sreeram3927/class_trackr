cfa6693522393c134888a51e8e5be38f6a595090
Sreeram3927 <sreeram292004@gmail.com>
2024-01-09 21:21:27 +0530

    create timetable and course model

---
+++ lib/models/course.dart
@@ -0,0 +1,13 @@
+
+class Course {
+  
+  final String name;
+  final String code;
+  final String faculty;
+
+  const Course({
+    required this.name,
+    this.code = '',
+    this.faculty = '',
+  });
+}
